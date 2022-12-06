036fd588090d05da03e431b2bf46c1445c83446f
JastonRuggy <jastonruggy@icloud.com>
2022-12-06 18:49:44 +0300

    ef

---
+++ src/test/java/ruggy/RuggyApplicationTests.java
@@ -0,0 +1,13 @@
+package ruggy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuggyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
