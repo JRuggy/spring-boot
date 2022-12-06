976b7b853dc54f25e3c3ac06fd96be7d307873d3
JastonRuggy <jastonruggy@icloud.com>
2022-12-06 18:59:40 +0300

    efe

--- src/main/java/ruggy/RuggyApplication.java
+++ src/main/java/ruggy/RuggyApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+// import antlr.collections.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +16,8 @@ public class RuggyApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Jaston Ruggy";
+	public List<String> hello() {
+		return List.of("Jaston","Ruggy");
 	}
 
 }
