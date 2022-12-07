26c01942b09b515c1bfaf3cce88fb0f69c0308ab
JastonRuggy <jastonruggy@icloud.com>
2022-12-07 17:56:00 +0300

    efef

--- src/main/java/ruggy/student/StudentRepository.java
+++ src/main/java/ruggy/student/StudentRepository.java
@@ -3,11 +3,13 @@ package ruggy.student;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long>{
 
+    // @Query("SELECT s FROM Student s WHERE s.email = ?1")      This is JPQuery and not a straight query mind you
     // select from student where email = ?
     Optional<Student> findStudentByEmail(String email);
 }
