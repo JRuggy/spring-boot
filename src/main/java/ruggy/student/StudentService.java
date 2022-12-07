be9afc724e00c97be88cbefdffaf7dec9b3abc63
JastonRuggy <jastonruggy@icloud.com>
2022-12-07 18:27:27 +0300

    ef

--- src/main/java/ruggy/student/StudentService.java
+++ src/main/java/ruggy/student/StudentService.java
@@ -1,6 +1,7 @@
 package ruggy.student;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,15 +12,21 @@ public class StudentService {
     private final StudentRepository studentRepository;
 
     @Autowired
-    public StudentService(StudentRepository studentRepository){
+    public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
     public List<Student> getStudents() {
-		return studentRepository.findAll();
-	}
+        return studentRepository.findAll();
+    }
 
-    public void addNewStudent(Student student){
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        // condition to check if the data in variable email is existing
+        if(studentOptional.isPresent()){
+            throw new IllegalStateException("Email's taken");
+        }
         System.out.println(student);
     }
 }
